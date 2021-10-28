c511474ff1daccc340a72b7799ca6b396b8d9551
Bernardo Cardoso <bernardo@xx.network>
2021-10-29 00:13:07 +0200

    Add compilation for ARM linux

--- cli-generator/build.sh
+++ cli-generator/build.sh
@@ -5,6 +5,8 @@ echo "==> Building Sleeve CLI for Mac M1"
 GOOS=darwin GOARCH=arm64 go build -o bin/sleeve-mac-m1
 echo "==> Building Sleeve CLI for Linux"
 GOOS=linux GOARCH=amd64 go build -o bin/sleeve-linux
+echo "==> Building Sleeve CLI for ARM Linux"
+GOOS=linux GOARCH=arm64 go build -o bin/sleeve-arm-linux
 echo "==> Building Sleeve CLI for Windows"
 GOOS=windows GOARCH=amd64 go build -o bin/sleeve-windows.exe
 shasum -a 256 bin/*
--- sleevage/build.sh
+++ sleevage/build.sh
@@ -5,6 +5,8 @@ echo "==> Building sleevage CLI for Mac M1"
 GOOS=darwin GOARCH=arm64 go build -o bin/sleevage-mac-m1
 echo "==> Building sleevage CLI for Linux"
 GOOS=linux GOARCH=amd64 go build -o bin/sleevage-linux
+echo "==> Building sleevage CLI for ARM Linux"
+GOOS=linux GOARCH=arm64 go build -o bin/sleevage-arm-linux
 echo "==> Building sleevage CLI for Windows"
 GOOS=windows GOARCH=amd64 go build -o bin/sleevage-windows.exe
 shasum -a 256 bin/*
