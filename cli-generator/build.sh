5ec42c542053286277fec20f470d68ae910193de
Bernardo Cardoso <bernardo@xx.network>
2021-11-09 22:19:02 +0100

    Add ARM 32 bit compilation of sleeve and sleevag

--- cli-generator/build.sh
+++ cli-generator/build.sh
@@ -5,8 +5,10 @@ echo "==> Building Sleeve CLI for Mac M1"
 GOOS=darwin GOARCH=arm64 go build -o bin/sleeve-mac-m1
 echo "==> Building Sleeve CLI for Linux"
 GOOS=linux GOARCH=amd64 go build -o bin/sleeve-linux
-echo "==> Building Sleeve CLI for ARM Linux"
+echo "==> Building Sleeve CLI for ARM (64-bit) Linux"
 GOOS=linux GOARCH=arm64 go build -o bin/sleeve-arm-linux
+echo "==> Building Sleeve CLI for ARM (32-bit) Linux"
+GOOS=linux GOARCH=arm go build -o bin/sleeve-arm-32bit-linux
 echo "==> Building Sleeve CLI for Windows"
 GOOS=windows GOARCH=amd64 go build -o bin/sleeve-windows.exe
 shasum -a 256 bin/*
--- sleevage/build.sh
+++ sleevage/build.sh
@@ -5,8 +5,10 @@ echo "==> Building sleevage CLI for Mac M1"
 GOOS=darwin GOARCH=arm64 go build -o bin/sleevage-mac-m1
 echo "==> Building sleevage CLI for Linux"
 GOOS=linux GOARCH=amd64 go build -o bin/sleevage-linux
-echo "==> Building sleevage CLI for ARM Linux"
+echo "==> Building sleevage CLI for ARM (64-bit) Linux"
 GOOS=linux GOARCH=arm64 go build -o bin/sleevage-arm-linux
+echo "==> Building sleevage CLI for ARM (32-bit) Linux"
+GOOS=linux GOARCH=arm go build -o bin/sleevage-arm-32bit-linux
 echo "==> Building sleevage CLI for Windows"
 GOOS=windows GOARCH=amd64 go build -o bin/sleevage-windows.exe
 shasum -a 256 bin/*
